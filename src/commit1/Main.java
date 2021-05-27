def7fb69812405b6c42c50e1844e626ec0c4b525
QWEQWE <Q@gm.com>
2021-05-27 11:06:20 +0300

    commit 4 with changed name MERGED SQUASHED
    commit 2 branch
    
    commit 3 branch

--- src/commit1/Main.java
+++ src/commit1/Main.java
@@ -2,6 +2,6 @@ package commit1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("checkcommit4");
+        System.out.println("Check branch 3");
     }
 }
